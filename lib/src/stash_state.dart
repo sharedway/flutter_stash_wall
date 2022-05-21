981a2bd8541a9b87ef50a14d87b0230125669411
Lauro C Oliveira <lauro@hostcert.com.br>
2022-05-21 12:39:53 -0300

    Added config gameid

--- lib/src/stash_state.dart
+++ lib/src/stash_state.dart
@@ -4,6 +4,16 @@ import 'package:stash_wall/src/notifiers/generic_rest_and_socket_notifier.dart';
 class StashState extends GenericRestAndSocketNotifier {
   StashState({required String keyName, required Map<String, dynamic> config}) : super(keyName: keyName, config: config);
 
+  bool _isReady = false;
+
   @override
-  bool get isReady => false;
+  Future<void> onInit() async {
+    Future.delayed(Duration(seconds: 20), () {
+      _isReady = true;
+      notifyListeners();
+    });
+  }
+
+  @override
+  bool get isReady => _isReady;
 }
