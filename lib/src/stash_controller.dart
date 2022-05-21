f85486b0f6f1b4a6eeb3697610b8bd9e06beff4e
Lauro C Oliveira <lauro@hostcert.com.br>
2022-05-21 19:10:15 -0300

    Added overlay mode

--- lib/src/stash_controller.dart
+++ lib/src/stash_controller.dart
@@ -21,8 +21,6 @@ class _StashControllerState extends State<StashController> {
       width: w,
       height: h,
       child: Stack(
-        fit: StackFit.expand,
-        textDirection: TextDirection.rtl,
         children: [
           widget.rootWidget,
           Positioned(
@@ -48,7 +46,7 @@ class _StashControllerState extends State<StashController> {
           AnimatedPositioned(
               right: 0,
               left: 0,
-              top: (context.watch<StashState>().show) ? 0 : (500),
+              top: (context.watch<StashState>().show) ? 0 : (h),
               bottom: 0,
               duration: const Duration(milliseconds: 300),
               curve: Curves.easeInOutQuad,
