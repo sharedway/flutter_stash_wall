1aa6d2aaea264264e2d00ca33fe26d23edfd0911
Lauro C Oliveira <lauro@hostcert.com.br>
2022-05-21 18:28:46 -0300

    Added material size

--- lib/src/stash_controller.dart
+++ lib/src/stash_controller.dart
@@ -14,52 +14,55 @@ class StashController extends StatefulWidget {
 class _StashControllerState extends State<StashController> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.center,
-      fit: StackFit.expand,
-      children: [
-        widget.rootWidget,
-        Positioned(
-            bottom: 10,
-            left: 10,
-            right: 10,
-            child: Container(
-              color: Colors.indigo,
-              child: Row(
-                children: [
-                  const Text(
-                    "teste",
-                    style: TextStyle(fontSize: 18, color: Colors.amber),
-                  ),
-                  TextButton(
-                      onPressed: () {
-                        context.read<StashState>().showOverlay();
-                      },
-                      child: const Text("Show")),
-                ],
-              ),
-            )),
-        AnimatedPositioned(
-            right: 0,
-            left: 0,
-            top: (context.watch<StashState>().show) ? 0 : (MediaQuery.of(context).size.height),
-            bottom: 0,
-            duration: const Duration(milliseconds: 300),
-            curve: Curves.easeInOutQuad,
-            child: Container(
-              color: Colors.black.withOpacity(0.6),
-              alignment: Alignment.center,
-              child: ListView(
-                children: [
-                  TextButton(
-                      onPressed: () {
-                        context.read<StashState>().hideOverlay();
-                      },
-                      child: Text("Hide"))
-                ],
-              ),
-            )),
-      ],
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          widget.rootWidget,
+          Positioned(
+              bottom: 10,
+              left: 10,
+              right: 10,
+              child: Container(
+                color: Colors.indigo,
+                child: Row(
+                  children: [
+                    const Text(
+                      "teste",
+                      style: TextStyle(fontSize: 18, color: Colors.amber),
+                    ),
+                    TextButton(
+                        onPressed: () {
+                          context.read<StashState>().showOverlay();
+                        },
+                        child: const Text("Show")),
+                  ],
+                ),
+              )),
+          AnimatedPositioned(
+              right: 0,
+              left: 0,
+              top: (context.watch<StashState>().show) ? 0 : (MediaQuery.of(context).size.height),
+              bottom: 0,
+              duration: const Duration(milliseconds: 300),
+              curve: Curves.easeInOutQuad,
+              child: Container(
+                color: Colors.black.withOpacity(0.6),
+                alignment: Alignment.center,
+                child: ListView(
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          context.read<StashState>().hideOverlay();
+                        },
+                        child: Text("Hide"))
+                  ],
+                ),
+              )),
+        ],
+      ),
     );
   }
 }
