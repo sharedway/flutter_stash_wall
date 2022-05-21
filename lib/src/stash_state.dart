66ed4fe290cdfcd86551e9a98764d9e6722397d1
Lauro C Oliveira <lauro@hostcert.com.br>
2022-05-21 11:59:21 -0300

    Added overlay

--- lib/src/stash_state.dart
+++ lib/src/stash_state.dart
@@ -3,4 +3,7 @@ import 'package:stash_wall/src/notifiers/generic_rest_and_socket_notifier.dart';
 
 class StashState extends GenericRestAndSocketNotifier {
   StashState({required String keyName, required Map<String, dynamic> config}) : super(keyName: keyName, config: config);
+
+  @override
+  bool get isReady => false;
 }
